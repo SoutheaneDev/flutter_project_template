d957ad07afd20436e9951e5811e3dd6df73100e0
southeane <southeane@southeanes-MacBook-Pro.local>
2026-02-16 00:50:01 +0700

    Configure Theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -5,6 +5,7 @@ import 'package:rental_room/utils/theme/custom_themes/checkbox_theme.dart';
 import 'package:rental_room/utils/theme/custom_themes/chip_theme.dart';
 import 'package:rental_room/utils/theme/custom_themes/elevated_button_theme.dart';
 import 'package:rental_room/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:rental_room/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:rental_room/utils/theme/custom_themes/text_theme.dart';
 
 class TAppTheme {
@@ -16,13 +17,14 @@ class TAppTheme {
     brightness: Brightness.light,
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
-    textTheme: TTextTheme.lightTextTheme,
-    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
     appBarTheme: TAppbarTheme.lightAppBarTheme,
     bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
     checkboxTheme: TCheckboxTheme.lightCheckboxTheme,
     chipTheme: TChipTheme.lightChipTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
     outlinedButtonTheme: TOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: TTextFieldTheme.lightTextFieldTheme,
+    textTheme: TTextTheme.lightTextTheme,
   );
   static final ThemeData darkTheme = ThemeData(
     useMaterial3: true,
@@ -30,12 +32,13 @@ class TAppTheme {
     brightness: Brightness.dark,
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
-    textTheme: TTextTheme.darkTextTheme,
-    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
     appBarTheme: TAppbarTheme.darkAppBarTheme,
     bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
     checkboxTheme: TCheckboxTheme.darkCheckboxTheme,
     chipTheme: TChipTheme.darkChipTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
     outlinedButtonTheme: TOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: TTextFieldTheme.darkTextFieldTheme,
+    textTheme: TTextTheme.darkTextTheme,
   );
 }
